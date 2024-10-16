7c3c9dbb28743eecef4f1e02470f3317821ba209
rbazur <razur@udm.edu.ph>
2024-10-16 06:39:01 -0700

    pagination

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -15,7 +15,7 @@ class ListingController extends Controller
         // dd(request()->tag);
         return view('listings.index', [
             'heading' => 'Latest Listings',
-            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(4)
         ]);
     }
 
